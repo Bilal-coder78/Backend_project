a97193260c54132714b12f6c33fc46943dc170a0
M Bilal <bilalsikander87@gmail.com>
2026-01-26 11:27:42 +0500

    get user details and validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,37 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import { User } from '../models/user.model.js';
 
-
-export const registerUser = asyncHandler( async (req,res) => {
+export const registerUser = asyncHandler(async (req, res) => {
     // Registration logic here
-    res.status(200).json({
-        message: "Bilal is the user",
+    
+    // get user details from fronted
+    // validation - not empty
+    // check if user already exists : email,username
+    // check for images check for avatar
+    // upload them to cloudinary,avatar
+    // create user object  - create entry in db
+    // remove password and refresh token from response
+    // check for user creation
+    // return response
+
+    const { username, email, fullname, password } = req.body;
+    console.log("email :", email)
+
+    // validation
+    if ([username, email, fullname, password].some((field) =>
+        field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+    if(!email.includes("@")){
+        throw new ApiError(400, "Invalid email format")
+    }
+
+    // check if user already exists
+    const existedUser = User.findOne({
+        $or: [{email}, {username}]
     })
+    if(existedUser){
+        throw new ApiError(409, "User already exists with this email or username")
+    }
 })
