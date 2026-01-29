31d63007d6257d8ea0ac24242fba0e4da3f0d70d
M Bilal <bilalsikander87@gmail.com>
2026-01-29 11:32:37 +0500

    generate access and refresh tokens and user loggedIn

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,24 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
+const generateAccessAndRefreshToken = async (userid) => {
+    try {
+        const user = await User.findById(userid);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        // save tokens in db
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false});
+
+        return {accessToken, refreshToken};
+
+    } catch (error) {
+        throw new ApiError (500, "Something went wrong while generating refresh and access tokens")
+    }
+
+}
+
 export const registerUser = asyncHandler(async (req, res) => {
     // Registration logic here
 
@@ -25,15 +43,15 @@ export const registerUser = asyncHandler(async (req, res) => {
         field?.trim() === "")) {
         throw new ApiError(400, "All fields are required");
     }
-    if(!email.includes("@")){
+    if (!email.includes("@")) {
         throw new ApiError(400, "Invalid email format")
     }
 
     // check if user already exists
     const existedUser = await User.findOne({
-        $or: [{email}, {username}]
+        $or: [{ email }, { username }]
     })
-    if(existedUser){
+    if (existedUser) {
         throw new ApiError(409, "User already exists with this email or username")
     }
     // console.log(req.files)
@@ -42,11 +60,11 @@ export const registerUser = asyncHandler(async (req, res) => {
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
 
-    if(!avatarLocalPath){
+    if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required")
     }
 
@@ -74,7 +92,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     )
 
     // check for user creation
-    if(!createdUser){
+    if (!createdUser) {
         throw new ApiError(500, "Something went wrong while creating a user")
     }
 
@@ -82,4 +100,60 @@ export const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(
         new ApiResponse(201, "User registered successfully", createdUser)
     )
+})
+
+export const loginUser = asyncHandler(async (req, res) => {
+    // req body - data
+    // email or username
+    // find the user
+    // password check
+    // generate access and refresh token
+    // send cookies
+
+    const { username, email, password } = req.body;
+
+    if (!email || !username) {
+        throw new ApiError(400, "Email or username is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{ email }, { username }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    // send cookies
+
+    // method for security changes can only be set in server not in frontend
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("refreshToken",refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+        new ApiResponse(200,
+            {
+                user: loggedInUser,accessToken, refreshToken
+            },
+            "User logged in Successfully"
+        )
+    )
+
 })
